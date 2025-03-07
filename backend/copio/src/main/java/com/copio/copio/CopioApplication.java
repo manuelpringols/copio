0481f31426dfa5d255632f3075dccad8a64f6fc9
manuelpringols <manuelpringols@gmail.com>
2025-03-07 22:06:31 +0100

    bozza backend copio

---
+++ backend/copio/src/main/java/com/copio/copio/CopioApplication.java
@@ -0,0 +1,13 @@
+package com.copio.copio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopioApplication.class, args);
+	}
+
+}
---
+++ backend/copio/src/test/java/com/copio/copio/CopioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copio.copio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
