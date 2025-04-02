c5850449481f433c59809fdbc72b768c337ef79d
manuelpringols <manuelpringols@gmail.com>
2025-04-02 17:34:57 +0200

    estensione deployata con link di download sul sito rev:1.0

--- backend/copio/src/main/java/com/copio/copio/security/SecurityConfig.java
+++ backend/copio/src/main/java/com/copio/copio/security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://copio.online","http://localhost:4200","chrome-extension://kmgclaebnbboaofefadmliagoaogekpen" //
+        configuration.setAllowedOrigins(List.of("https://copio.online","http://localhost:4200","chrome-extension://kmgclaebnbboaofefadmliagoaogekpe" //
                         )); // Modifica l'origine se necessario
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
