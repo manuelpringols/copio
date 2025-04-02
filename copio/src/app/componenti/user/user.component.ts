54646a2f9e6b4ee62b4e2df1b06a723f3881fb49
manuelpringols <manuelpringols@gmail.com>
2025-04-02 14:46:18 +0200

    aggiunte e modifiche estetiche front-end

--- copio/src/app/componenti/user/user.component.ts
+++ copio/src/app/componenti/user/user.component.ts
@@ -8,12 +8,25 @@ import { Router } from '@angular/router';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+someAction() {
+throw new Error('Method not implemented.');
+}
   isMenuOpen = false;
 
   constructor(private router: Router) {}
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+    
+    // Animazione aggiuntiva per l'icona
+    const icon = document.querySelector('.logout-icon');
+    if (icon) {
+      if (this.isMenuOpen) {
+        icon.classList.add('active');
+      } else {
+        icon.classList.remove('active');
+      }
+    }
   }
 
   logout() {
